f4f94f9b8df16cb37f81e45b0c63a3c6ad4c74d6
Raghavmunjal <rghvmunjal@gmail.com>
2024-06-17 21:46:59 +0530

    added navigation controller

--- LoveChat/Controllers/RegisterController.swift
+++ LoveChat/Controllers/RegisterController.swift
@@ -5,4 +5,13 @@
 //  Created by Raghav Munjal on 17/06/24.
 //
 
-import Foundation
+import UIKit
+
+class RegisterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
